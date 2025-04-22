6f113285bcce16545225b6dfa48381bb05acb0ed
KaiNabea <n01722652@humber.ca>
2025-04-22 18:17:02 -0400

    addingGroupProject

---
+++ group-project/src/app/page.js
@@ -0,0 +1,31 @@
+'use client'
+
+import React, { useState } from "react";
+import styles from "./page.module.css"
+import Link from "next/link"
+import Footer from "../components/Footer";
+
+export default function Home() {
+  const [title, setTitle] = useState("Home");
+
+  return (
+    <div className = {styles.page} style={{ margin: '24px' }}>
+      <h1 style = {{textShadow: '2px 2px black', fontSize: '60px', textAlign: 'center', padding: '4px' }}>DOJI'S</h1>
+      <h2 style = {{textShadow: '2px 2px black', fontSize: '24px', textAlign: 'center', padding: '4px'}}><i>Bold, Exquisite, Marvelous</i></h2>
+      <h3 style = {{textShadow: '2px 2px black', fontSize: '20px', textAlign: 'center', padding: '4px'}}>
+        <Link href = "/Menu">
+          Menu
+        </Link>
+        <br/><br/>
+        <Link href = "/About">
+          About
+        </Link>
+        <br/><br/>
+        <Link href = "/Contact">
+          Book Now
+        </Link>
+      </h3>
+      <Footer />
+    </div>
+  );
+}
